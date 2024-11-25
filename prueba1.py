08185e62891ee173fd599a036beec46a7c2471cb
LuisCalle17 <nyxluis17@gmail.com>
2024-11-25 10:33:53 -0500

    Ejercicios Realizados 1

---
+++ prueba1.py
@@ -0,0 +1,130 @@
+"""
+Ejercicio 1: Escribe un programa que solicite al usuario dos números y muestre su
+suma, resta, multiplicación, división, división entera y residuo (módulo).
+"""
+
+# num1 = int(input("Ingrese el primero numero: "))
+# num2 = int(input("Ingrese el segundo numero: "))
+
+# suma = num1 + num2
+# resta = num1 - num2
+# multiplicacion = num1 * num2
+# division = num1 / num2 if num2 != 0 else "No se puede dividir entre 0"
+# division_entera = num1 // num2 if num2 != 0 else "No se puede dividir entre 0"
+# residuo = num1 % num2 if num2 != 0 else "No se puede dividir entre 0"
+
+# print(f"Suma: {suma}")
+# print(f"Resta: {resta}")
+# print(f"Multiplicación: {multiplicacion}")
+# print(f"División: {division}")
+# print(f"División entera: {division_entera}")
+# print(f"Residuo: {residuo}")
+
+
+"""
+Ejercicio 2: Escribe un programa que solicite al usuario un número entero y calcule
+su cuadrado y su cubo.
+"""
+
+# numero = int(input("Ingrese un número entero: "))
+
+# cuadrado = numero **2
+# cubo = numero **3
+
+# print(f"El cuadrado del numero: {cuadrado}")
+# print(f"El cubo del numero: {cubo}")
+
+
+"""
+Ejercico 3: Escribe un programa que lea un número entero y determine si es
+positivo, negativo o cero.
+"""
+
+# numero = int(input("Ingrese un número: "))
+
+# if numero > 0:
+#     print(f"El número {numero} es positivo.")
+# elif numero < 0:
+#     print(f"El número {numero} es negativo.")
+# else:
+#     print("Es cero.")
+
+
+"""
+Ejercico 4: Escribe un programa que solicite al usuario un número entero y calcule
+si es divisible por 3 y por 5.
+"""
+
+# numero = int(input("Ingrese un número entero: "))
+
+# if numero % 3 == 0 and numero % 5 == 0:
+#     print("Es divisible por 3 y por 5")
+# elif numero % 3 == 0:
+#     print("Es divisible por 3")
+# elif numero % 5 == 0:
+#     print("Es divisible por 5")
+# else:
+#     print("No es divisible por 3 ni por 5")
+
+
+"""
+Ejercico 5: Escribe un programa que tome una calificación numérica de un
+estudiante (entre 0 y 100) y le asigne una letra según la siguiente tabla:
+
+- 90-100: A
+- 80-89: B
+- 70-79: C
+- 60-69: D
+- Menos de 60: F    
+
+"""
+
+# calificacion = int(input("Ingrese calificación entre 0 y 100: "))
+
+# if 90 <= calificacion <= 100:
+#     print("A")
+# elif 80 <= calificacion <= 89:
+#     print("B")
+# elif 70 <= calificacion <= 79:
+#     print("C")
+# elif 60 <= calificacion <= 69:
+#     print("D")
+# elif calificacion < 60:
+#     print("F")
+# else:
+#     print("Ingresaste una calificación incorrecta")
+
+
+
+"""
+Ejercicio 6: Escribe un programa que solicite al usuario tres números y determine
+cuál de ellos es el mayor.
+
+"""
+# numero1 = int(input("Ingrese primer número: "))
+# numero2 = int(input("Ingrese segundo número: "))
+# numero3 = int(input("Ingrese tercer número: "))
+
+# if numero1 > numero2:
+#     print(f"{numero1} es mayor")
+# elif  numero2 > numero3:
+#     print(f"{numero2} es mayor")
+# elif numero3 > numero1:
+#     print(f"{numero3} es mayor")
+# else:
+#     print("Son iguales")
+
+
+"""
+Ejercico 7: Escribe un programa que solicite al usuario un número entero y
+determine si es par o impar.
+"""  
+
+# numero = int(input("Ingrese numero entero: "))
+
+# if numero % 2 == 0:
+#     print("El  número es par.")
+# else:
+#     print("El número es impar")
+
+
